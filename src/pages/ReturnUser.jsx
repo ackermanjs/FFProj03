df12e9050d08a03782bb565f9a591b08728c77f0
Jeffrey Ackerman <ackerman.jeffrey@gmail.com>
2023-03-08 11:18:53 -0700

    sign up and sign in code is causing a number of errors so i've commented it out for now to get the app to load

--- src/pages/ReturnUser.jsx
+++ src/pages/ReturnUser.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import "../components/ReturnUser.css";
-import SignIn from "./components/SignIn";
+// import SignIn from "./SignIn";
 
 const ReturnUser = () => {
   return (
 <div>
       <div className="returninguser-section">
-        <div className="returninguser-container"><SignIn /></div>
+        {/* <div className="returninguser-container"><SignIn /></div> */}
         <div className="returninguser-text-container">
           <h1 className="returninguser-title"></h1>
         </div>
