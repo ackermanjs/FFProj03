7b36dbe7f0761e1f7be094f70eac9e58208ebb3c
Jeffrey Ackerman <ackerman.jeffrey@gmail.com>
2023-03-08 10:38:48 -0700

    working on sign in sign up functions

--- src/pages/NewUser.jsx
+++ src/pages/NewUser.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import "../components/NewUser.css";
+import SignUp from "./components/SignUp";
 
 const NewUser = () => {
   return (
 <div>
       <div className="newuser-section">
-        <div className="newuser-container"></div>
+        <div className="newuser-container"><SignUp /></div>
         <div className="newuser-text-container">
           <h1 className="newuser-title"></h1>
         </div>
