254a08ec04af4354afcf3cfaea5fc60a9d1b3d87
Jeffrey Ackerman <ackerman.jeffrey@gmail.com>
2023-03-08 12:47:33 -0700

    adjusting json files, modules, scripts, STABLE

---
+++ Client/src/pages/About.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../components/About.css";
+
+const About = () => {
+  return (
+    <div>
+      <div className="about-section">
+        <div className="about-container"></div>
+        <div className="about-text-container">
+          <h1 className="about-title"></h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ Client/src/pages/ReturnUser.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "../components/ReturnUser.css";
+// import SignIn from "./SignIn";
+
+const ReturnUser = () => {
+  return (
+<div>
+      <div className="returninguser-section">
+        {/* <div className="returninguser-container"><SignIn /></div> */}
+        <div className="returninguser-text-container">
+          <h1 className="returninguser-title"></h1>
+        </div>
+      </div>
+    </div>  )
+}
+
+export default ReturnUser
