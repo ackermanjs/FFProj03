efcc68db1608e9f0dca184c3b60c24255f0d8d82
austin bennett <awb157@gmail.com>
2023-03-07 20:11:53 -0700

    cut apollo on line72

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import Button from "react-bootstrap/Button"
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// commemted out by Austin
+// import {
+//   ApolloClient,
+//   InMemoryCache,
+//   ApolloProvider,
+//   createHttpLink,
+// } from '@apollo/client';
+// import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter , Routes, Route } from 'react-router-dom';
 
 import TopNav from "./components/TopNav";
 import Header from "./components/Header";
@@ -19,42 +21,55 @@ import Footer from "./components/Footer";
 import Dogs from './pages/Dogs';
 import Cats from './pages/Cats';
 
+ import SignUp from "./pages/SignUp"
+import SignIn from "./pages/SignIn"
+// commemted out by Austin
 // import Footer from './components/Footer';
-const httpLink = createHttpLink({
-  uri: '/graphql',
-});
+// const httpLink = createHttpLink({
+//   uri: '/graphql',
+// });
 
-// Construct request middleware that will attach the JWT token to every request as an `authorization` header
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('id_token');
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
+// // Construct request middleware that will attach the JWT token to every request as an `authorization` header
+// const authLink = setContext((_, { headers }) => {
+//   // get the authentication token from local storage if it exists
+//   const token = localStorage.getItem('id_token');
+//   // return the headers to the context so httpLink can read them
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : '',
+//     },
+//   };
+// });
 
-const client = new ApolloClient({
-  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
+// const client = new ApolloClient({
+//   // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+//   link: authLink.concat(httpLink),
+//   cache: new InMemoryCache(),
+// });
 
 function App() {
   return (
     <div className="App">
-      <ApolloProvider client={client}>
+       {/* commemted out by Austin */}
+      {/* <ApolloProvider client={client}> */}
+      {/* <BrowserRouter/> */}
       <Header />
+      {/* <Routes> */}
+
+        {/* cut for now by Austin */}
+{/* <Route path="/signup" element={<SignUp/>}/>  */}
+{/* cut by Austin */}
+{/* <Route path="/signin" element={<SignIn/>}/> */}
+{/* <Route path="/dogs" element={<Dogs/>}/> */}
+{/* <Route path="/cats" element={<Cats/>}/> */}
+        {/* </Routes> */}
       <HeroSection />
       <Dogs />
       <Cats />
       <BodyMid />
       <Body />
-      <Footer />
-      </ApolloProvider>
+      {/* </ApolloProvider> */}
     </div>
   );
 }
--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -4,14 +4,52 @@ import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Button from "react-bootstrap/Button";
 import "./TopNav.css";
-// import NavLink from "react";
-// import BrandLogo from "../images/pet-house.png";
+import Auth from '../utils/auth';
 import {Link}from "react-router-dom"
 const TopNav = () => {
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
   return (
-    <>
-      
-    </>
+    <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
+    <div className="container flex-row justify-space-between-lg justify-center align-center">
+      <div>
+        <Link className="text-light" to="/">
+          <h1 className="m-0">Furever Friends</h1>
+        </Link>
+        <p className="m-0">No Pet Left Behind!</p>
+      </div>
+      <div>
+        {Auth.loggedIn() ? (
+          <>
+            <Link className="btn btn-lg btn-info m-2" to="/me">
+              {Auth.getProfile().data.username}'s profile
+            </Link>
+            <button className="btn btn-lg btn-light m-2" onClick={logout}>
+              Logout
+            </button>
+          </>
+        ) : (
+          <>
+ <Link className="btn btn-lg btn-info m-2" to="/dogs">
+              Dogs
+            </Link>
+            <Link className="btn btn-lg btn-info m-2" to="/cats">
+              cats
+            </Link>
+
+            <Link className="btn btn-lg btn-info m-2" to="/login">
+              Login
+            </Link>
+            <Link className="btn btn-lg btn-light m-2" to="/signup">
+              Signup
+            </Link>
+          </>
+        )}
+      </div>
+    </div>
+  </header>
   );
 };
 
