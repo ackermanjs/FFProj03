bef53fcaec77ede971a0585bcc0da68fb3bd6161
Rhondoid <110504360+Rhondoid@users.noreply.github.com>
2023-03-09 20:50:09 -0700

    fixed typefs schema server.js

--- server/models/Animals.js
+++ server/models/Animals.js
@@ -1,5 +1,5 @@
 const { Schema, model } = require('mongoose');
-const dateFormat = require('../utils/dateFormat');
+// const dateFormat = require('../utils/dateFormat');
 
 const animalSchema = new Schema({
   name: {
@@ -30,11 +30,11 @@ const animalSchema = new Schema({
         type: String,
         required: true,
       },
-      createdAt: {
-        type: Date,
-        default: Date.now,
-        get: (timestamp) => dateFormat(timestamp),
-      },
+      // createdAt: {
+      //   type: Date,
+      //   default: Date.now,
+      //   get: (timestamp) => dateFormat(timestamp),
+      // },
     },
   ],
 });
